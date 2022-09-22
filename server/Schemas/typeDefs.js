50afc97464393f0ddb7d93da6d7730f461849619
Travis Ramcharran <tc.ramcharran@gmail.com>
2022-09-21 21:28:48 -0500

    added type-defs, add_user mutation, finished resolvers.js?

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+_id: ID
+username: String
+email: String
+bookCount: Int
+savedBooks: [Book]
+}
+type Book {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+type Auth {
+    token: ID!
+    user: User
+}
+type Query {
+    me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: String!): User
+}
+input BookInput {
+    authors: [String]
+    bookId: String
+    description: String
+    title: String
+    image: String
+    link: String
+}
+`
+
+module.exports = typeDefs;
